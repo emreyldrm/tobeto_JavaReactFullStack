e01d21d17002c0e833f98445677fafd11191d9df
Emre <emreyldrm80@gmail.com>
2023-10-29 00:38:52 +0300

    car entity created

---
+++ Workshop/W3_27.10.2023/Work1/rentACar_exercise/src/main/java/org/example/entities/Car.java
@@ -0,0 +1,80 @@
+package org.example.entities;
+
+public class Car {
+    private int id;
+    private String brand;
+    private String model;
+    private double dateOfManufacture;
+    private String gearType;
+    private String fuelType;
+    private double dailyRentCharge;
+
+    public Car() {
+
+    }
+    public Car(int id, String brand, String model, double dateOfManufacture, String gearType, String fuelType, double dailyRentCharge) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.dateOfManufacture = dateOfManufacture;
+        this.gearType = gearType;
+        this.fuelType = fuelType;
+        this.dailyRentCharge = dailyRentCharge;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getDateOfManufacture() {
+        return dateOfManufacture;
+    }
+
+    public void setDateOfManufacture(double dateOfManufacture) {
+        this.dateOfManufacture = dateOfManufacture;
+    }
+
+    public String getGearType() {
+        return gearType;
+    }
+
+    public void setGearType(String gearType) {
+        this.gearType = gearType;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public double getDailyRentCharge() {
+        return dailyRentCharge;
+    }
+
+    public void setDailyRentCharge(double dailyRentCharge) {
+        this.dailyRentCharge = dailyRentCharge;
+    }
+}
