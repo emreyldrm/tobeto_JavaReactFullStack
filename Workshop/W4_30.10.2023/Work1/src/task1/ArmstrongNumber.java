a05f45a8a9efabf4ffaba84f97d90a59998f5d3e
Emre <emreyldrm80@gmail.com>
2023-11-27 23:49:29 +0300

    w4 t1 done

--- Workshop/W4_30.10.2023/Work1/src/task1/ArmstrongNumber.java
+++ Workshop/W4_30.10.2023/Work1/src/task1/ArmstrongNumber.java
@@ -1,7 +1,40 @@
 package task1;
 
+import java.util.Scanner;
+
 public class ArmstrongNumber {
-    //receiving data from user
+    public static void main(String[] args) {
+
+        //getting a number
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter a number : ");
+        int number = input.nextInt();
+
+        //finding how many digits the entered number has
+        int length = String.valueOf(number).length();
+
+        //requirements
+        int originalNumber,remainder;
+        double result = 0;
+        originalNumber = number;
+
+        //armstrong finder
+        while(originalNumber != 0) {
+            remainder = originalNumber % 10;
+            result += Math.pow(remainder,length);
+            originalNumber = originalNumber / 10;
+        }
+
+        if (result == number){
+            System.out.println(number + " is an Armstrong number");
+        }
+        else{
+            System.out.println(number + " is not an Armstrong number");
+        }
+
+
+    }
+
 
 
 }
